bda74a6068fe963367a9cffbff5514193c1213fb
itsmevalenn <124251097+itsmevalenn@users.noreply.github.com>
2024-04-25 08:50:38 +0700

    Add files via upload

---
+++ ukk2024_arielvalen.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2024 at 06:47 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ukk2024_arielvalen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `AlbumID` int(11) NOT NULL,
+  `NamaAlbum` varchar(255) NOT NULL,
+  `Deskripsi` text NOT NULL,
+  `TanggalDibuat` date NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foto`
+--
+
+CREATE TABLE `foto` (
+  `FotoID` int(11) NOT NULL,
+  `JudulFoto` varchar(255) NOT NULL,
+  `DeskripsiFoto` text NOT NULL,
+  `TanggalUnggah` date NOT NULL,
+  `AlbumID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `foto`
+--
+
+INSERT INTO `foto` (`FotoID`, `JudulFoto`, `DeskripsiFoto`, `TanggalUnggah`, `AlbumID`, `UserID`) VALUES
+(0, 'foto/RobloxScreenShot20231206_164424741.png', 'halo', '0000-00-00', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentarfoto`
+--
+
+CREATE TABLE `komentarfoto` (
+  `KomentarID` int(11) NOT NULL,
+  `FotoID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `IsiKomentar` text NOT NULL,
+  `TanggalKomentar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likefoto`
+--
+
+CREATE TABLE `likefoto` (
+  `LikeID` int(11) NOT NULL,
+  `FotoID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `TanggalLike` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(225) NOT NULL,
+  `Password` varchar(225) NOT NULL,
+  `Email` varchar(225) NOT NULL,
+  `NamaLengkap` varchar(225) NOT NULL,
+  `Alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`UserID`, `Username`, `Password`, `Email`, `NamaLengkap`, `Alamat`) VALUES
+(7, 'deka', '123', 'deka@gmail.com', 'dekadarcho', 'jl.anjay'),
+(8, 'a', 'a', 'a@gmail.com', 'a', 'a'),
+(9, 'valen', '321', 'ariel321@gmail.com', 'arielvalensa', 'jl.pal3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`AlbumID`),
+  ADD UNIQUE KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `foto`
+--
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`FotoID`);
+
+--
+-- Indexes for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD PRIMARY KEY (`KomentarID`),
+  ADD UNIQUE KEY `FotoID` (`FotoID`,`UserID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD PRIMARY KEY (`LikeID`),
+  ADD UNIQUE KEY `FotoID` (`FotoID`,`UserID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD CONSTRAINT `komentarfoto_ibfk_1` FOREIGN KEY (`FotoID`) REFERENCES `foto` (`FotoID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `komentarfoto_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD CONSTRAINT `likefoto_ibfk_1` FOREIGN KEY (`FotoID`) REFERENCES `foto` (`FotoID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likefoto_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
